88fcb84bda9a3d1402ff7fdfe40a19cad6f97aff
shabdan_duishonaliev <shabdan.duishonaliev.01@gmail.com>
2024-11-16 16:25:42 +0600

    My project

--- backend/onlineshop/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnlineshopConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "onlineshop"
-
-    def ready(self):
-        import onlineshop.signals
